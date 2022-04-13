c53b97f0ffb08f2b3d20cb35e67afc693eb8e0c4
Milozap <mzapolski96@gmail.com>
2022-04-13 17:15:07 +0200

    Add user templates

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,6 @@
-import pytest
 from main import app
 
+
 def test_index():
-	response = app.test_client().get('/')
-	assert response.status_code == 200
-	assert response.text == "Index page"
+    response = app.test_client().get("/")
+    assert response.status_code == 200
